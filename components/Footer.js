3fabf315c70a6438d20e2af7255749b2fe0d899e
yuli-r <iuliana.rotaru234@gmail.com>
2024-02-18 21:33:11 +0000

    Add Footer component

---
+++ components/Footer.js
@@ -0,0 +1,59 @@
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <div>
+          <h2>Company</h2>
+          <p>About us</p>
+          <p>Contact us</p>
+          <p>Careers</p>
+          <p>Press</p>
+        </div>
+
+        <div>
+          <h2>Product</h2>
+          <p>Features</p>
+          <p>Pricing</p>
+          <p>News</p>
+          <p>Help desk</p>
+          <p>Support</p>
+        </div>
+
+        <div>
+          <h2>Services</h2>
+          <p>Digital Marketing</p>
+          <p>Content Writing</p>
+          <p>SEO for Business</p>
+          <p>UI Design</p>
+        </div>
+
+        <div>
+          <h2>Legal</h2>
+          <p>Privacy Policy</p>
+          <p>Terms & Conditions</p>
+          <p>Return Policy</p>
+        </div>
+
+        <div>
+          <h2>Contact us</h2>
+          <p>support@brainwave.io</p>
+          <p>+133-394-3439-1435</p>
+        </div>
+      </div>
+
+      <div>
+        <div>
+          2024 Copyright, All Right Reserved, Made by Seju_ui_ux with 💙
+        </div>
+        <div>
+          <img></img>
+          <img></img>
+          <img></img>
+          <img></img>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
