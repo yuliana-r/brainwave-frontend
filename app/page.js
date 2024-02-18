1463ada25e602995e00546951ab6869f8cf4154c
yuli-r <iuliana.rotaru234@gmail.com>
2024-02-18 21:18:25 +0000

    Add Features section and styling

--- app/page.js
+++ app/page.js
@@ -1,11 +1,13 @@
 import Navbar from "@/components/Navbar";
 import Pricing from "@/components/Pricing";
+import Features from "@/components/Features";
 
 export default function Home() {
   return (
     <>
       <Navbar />
       <Pricing />
+      <Features />
     </>
   );
 }
---
+++ components/Features.js
@@ -0,0 +1,17 @@
+const Features = () => {
+  return (
+    <div className="features">
+      <h1>Check our features</h1>
+      <p className="info">
+        With lots of unique blocks, you can easily build a page without coding.
+      </p>
+      <p className="info">
+        Build your next consultancy website within few minutes.
+      </p>
+
+      <div></div>
+    </div>
+  );
+};
+
+export default Features;
